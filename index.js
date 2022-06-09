7797ffbc6aad02edea42736cca2aab75ace0bcb8
NanaAwuku <apatrickadjei@gmail.com>
2022-06-09 07:06:26 +0000

    frst

---
+++ index.js
@@ -0,0 +1,12 @@
+const navSlider = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav_menu");
+  const navLinks = document.querySelectorAll(".nav_menu li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlider();
